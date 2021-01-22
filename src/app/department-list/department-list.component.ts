0242625d62d696de221451db48676886e9a5674c
Uttam Rawani <Uttam.Rawani@PROFINCHBLR.COM>
2021-01-22 19:30:03 +0530

    first commit

---
+++ src/app/department-list/department-list.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import {Router , ActivatedRoute , ParamMap} from '@angular/router'
+
+@Component({
+  selector: 'app-department-list',
+  template: `
+    <h3>
+      Department List
+</h3>
+<ul class="items">
+  <li (click)="onSelect(department)" [class.selected]="isSelected(department)" *ngFor="let department of departments">
+    <span class="badge"> {{department.id}} </span>{{department.name}}
+</li>
+</ul>
+  `,
+  styles: [
+  ]
+})
+export class DepartmentListComponent implements OnInit {
+  public selectedId;
+departments=[
+  {"id" :1,"name" :"Angular"},
+  {"id" :2,"name" :"Node"},
+  {"id" :3,"name" :"React"},
+  {"id" :4,"name" :"MongoDb"},
+  {"id" :4,"name" :"Bootstrap"}
+
+]
+  constructor(private router: Router , private route : ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((params : ParamMap)=>{
+      let id=parseInt(params.get('id'));
+      this.selectedId=id;
+          });
+  }
+
+  onSelect(department)
+  {
+    this.router.navigate(['/departments',department.id]);
+  }
+
+  isSelected(department)
+  {
+    return department.id===this.selectedId;
+  }
+
+}
